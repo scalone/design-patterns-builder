90f071a715cfbaa5ff830040d2fa9ff963bc6b4b
Thiago Scalone <thiago@scalone.com.br>
2019-03-07 11:27:03 -0300

    First Commit

---
+++ lib/design/patterns/builder.rb
@@ -0,0 +1,10 @@
+require "design/patterns/builder/version"
+
+module Design
+  module Patterns
+    module Builder
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/design/patterns/builder/version.rb
@@ -0,0 +1,7 @@
+module Design
+  module Patterns
+    module Builder
+      VERSION = "0.1.0"
+    end
+  end
+end
